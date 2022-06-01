886b7c1994639ed8dd9090c08b7dc4ae8ccf0520
hardik <hardickupadhyay008@gmail.com>
2022-06-02 00:15:23 +0530

    util

---
+++ src/logger/logger.js
@@ -0,0 +1,10 @@
+const endpoint = "https://www.functionup.org"
+const batch = "radon"
+
+const welcome = function() {
+    console.log('Welcome to my application. I am hardik and a part of FunctionUp radon cohort')
+}
+
+module.exports.endpoint = endpoint
+module.exports.batch = batch
+module.exports.log = welcome
---
+++ src/routes/routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const externalModule = require('../logger/logger.js')
+
+const router = express.Router();
+
+router.get('/test-me', function (req, res) {
+    console.log('The constant in logger route has a value '+externalModule.endpoint)
+    console.log('The current batch is '+externalModule.batch)
+    externalModule.log()
+    res.send('My first ever api!')
+});
+
+router.get('/test-me1', function (req, res) {
+    res.send('My second ever api!')
+});
+
+router.get('/test-me2', function (req, res) {
+    res.send('My third api!')
+});
+
+router.get('/test-me3', function (req, res) {
+    res.send('My 4th api!')
+});
+
+router.get('/test-me4', function (req, res) {
+    res.send('My last api!')
+});
+
+module.exports = router;
+// adding this comment for no reason
