6455a06836b54430725189ef320eb8fbe50c18ab
whohardik <hardickupadhyay008@gmail.com>
2022-07-06 15:12:23 -0700

    second commit

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const userController = require("../Controllers/userController")
+
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
+//---------------------Route For Creating Users-----------------------------------//
+router.post("/register", CollegeController.createCollege)
